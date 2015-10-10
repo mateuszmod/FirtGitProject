94f0089dabf60306d54af276f29b195b244663b8
mateuszmod <mateusz.modrzynski@gmail.com>
2015-10-10 19:32:21 +0200

    I changed the mainclass

--- FirstApp/src/MainClass.java
+++ FirstApp/src/MainClass.java
@@ -3,6 +3,8 @@
  */
 public class MainClass {
     public static void main(String[] args) {
+        String var = "works!";
         System.out.println("Hello world");
+        system.out.println("Second line " + var);
     }
 }
